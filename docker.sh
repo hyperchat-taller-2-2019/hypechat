d6c383887d9273035252e771f3209da3e1e81045
LeoCenturion <centurionm.leo@gmail.com>
2019-03-22 10:56:52 -0300

    Parametros agregados al script
    docker.sh ejecuta, con npm, el primer parametro pasado. Luego
    borra todos los contenedores de hypechat

--- docker.sh
+++ docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-PORT=8080
 
+# e.g. ./docker.sh command
+
+PORT=8080
 echo "########### Building hypechat ###########"
 echo ""
 sudo docker build -t hypechat . ;
 
 echo""; echo "########### Running hypechat ###########"
 echo "...Exposing port $PORT"; echo"";
-sudo docker run -p "$PORT":8080 hypechat;
+
+sudo docker run -p "$PORT":8080 hypechat npm "$1";
 
 echo"";echo "########### Removing containers ###########";echo"";
-sudo docker container rm $( sudo docker container ls -aq);
+sudo docker container rm $( sudo docker container ls -aq -f ancestor=hypechat);
