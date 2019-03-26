3c95f9d54814498a418607441e87b3218bca1ba5
LeoCenturion <centurionm.leo@gmail.com>
2019-03-26 16:23:33 -0300

    Server maneja varios end-points
    Tiene un ep '/' y otro '/login' . Solo devuelven texto

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const http = require('http')
+var mongo = require('mongodb')
+var MongoClient = mongo.MongoClient
+var url = "mongodb://127.0.0.1:27017/";
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
+
+var handleLogin = function(req,res){
+    res.write('login\n');
+};
+
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    switch(req.url) {
+    case '/login':
+	handleLogin(req,res);
+	break;
+    case '/':
+	res.write('home');
+	break;
+    default:
+	res.write('default');
+    } 
+
+    res.end();
+
+}).listen(PORT)
+
