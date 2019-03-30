af2fa93b654843a29647af9aeebc293eae595fe9
LeoCenturion <centurionm.leo@gmail.com>
2019-03-30 13:32:23 -0300

    Endpoint /registro funcionando.
    /registro funciona segun especificaciones de la api

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const async = require('async')
-const await = require('await')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const http = require('http')
@@ -8,8 +6,8 @@ var mongo = require('mongodb')
 var MongoClient = mongo.MongoClient
 const url = "mongodb://127.0.0.1:27017/";
 var express = require('express');
-var hypechat = express();
 
+var hypechat = express();
 hypechat.use(bodyParser.json());
 var jsonParser = bodyParser.json()
 
@@ -17,47 +15,37 @@ var server = hypechat.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
-    console.log("Example hypechat listening at http://%s:%s", host, port)
+    console.log("hypechat listening at http://%s:%s", host, port)
 
-})
+});
 
-var fetchUsers = function(err,db){
+var fetchUsers = function(db){
     let db_hypechat = db.db("hypechat");
     let users = db_hypechat.collection("users");
-    return users;
+    return users; 
 };
-var userExists = function(new_user,users){
 
-    users.findOne({"email": new_user.email}, function(err,index){
-	if(err) throw err;
-	if(index != null){
-	    return true;
-	}
-	return false;
-    })
-
-};
 var writeUser = function(new_user,res){
     MongoClient.connect(url,(err,db)=>{
-	const users = fetchUsers(err,db);
-	const alreadySingedUp = userExists(new_user,users);
-	console.log(alreadySingedUp);
-	if(alreadySingedUp == true){
-	    sendResponse(res,400,"{message:usuario ya registrado}");
-	}
-	else{
-	    users.insert(new_user, function(err,item ) {
-		if(err){
-		    sendResponse(res,400, "{message:Ha ocurrido un error}");
-		}
-		else{
-		    sendResponse(res,200, "todo piola");
-		}
-		
-	    });
-	
-	}
-	db.close();
+	if (err) throw err
+	var users = fetchUsers(db);
+	users.findOne({"email": new_user.email},(err,index)=>{
+	    if(err) throw err;
+	    if(index){
+		sendResponse(res,200,"{resultado: 0}");
+	    }
+	    else{
+		users.insertOne(new_user, function(err,item ){
+		    if(err){
+			console.log(err);
+			sendResponse(res,400, "{message:Ha ocurrido un error}");
+		    }
+		    else{
+			sendResponse(res,200, "{resultado: 1}");
+		    }
+		})
+	    }
+	});
     });
 };
 
@@ -74,13 +62,13 @@ hypechat.post('/registro', function (req, res) {
 
 hypechat.get('/users', function(req, res){
     MongoClient.connect(url, function(err, db){
-	let users = fetchUsers(err,db);
+	let users = fetchUsers(db);
 	users.find({}).toArray(function(err,result) {
 	    usersString = "";
 	    for (var i of result){
 		console.log(i.nombre);
 		usersString +=
-		    "{" + i.nombre   + "}";
+		    "{" + i.nombre   + "};";
 	    }
 
 	    sendResponse(res,200,usersString);
