805cb565f157222feb22b97750f930e6c4ec5a60
flortincab <flortincab@gmail.com>
2019-05-01 16:54:13 -0300

    pongo logs

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const service = require('../services')
 const User = require('../models/user');
-const logger = require('./utils/logger');
+const logger = require('../utils/logger');
 
 function getUser (req, res){
 	let userId = req.params.userId
@@ -68,31 +68,45 @@ function signUp(req,res){
 	const user = new User({
 		email: req.body.email,
 		name: req.body.name,
-		nickname: user.nickname,
+		nickname: req.body.nickname,
 		psw: req.body.psw,
 		photo : req.body.photo
 	})
 
 	user.save((err)=>{
-		if(err) return res.status(500).send({message: `Error al crear el usuario: ${err}`})
+		if(err) {
+			logger.error(`Error al crear el usuario ${user.email}: ${err}`)
+			return res.status(500).send({message: `singUp - Error al crear el usuario: ${err}`})
+		}
 		//res.status(200).send({token: service.createToken(user)})
+		logger.info(`signUp - Se creo el usuario con mail ${user.email}`)
 		res.status(200).send(user)
 	})
 }
 
 function logIn (req, res) {
 	User.findOne({ email: req.body.email, psw: req.body.psw }, (err, user) => {
-		if (err) return res.status(500).send({ message: `Error al ingresar mail: ${err}` })
-		if (!user) return res.status(404).send(
-			{ message: `El mail o la contraseña son invalidos`})
+		if (err) {
+			logger.error(`logIn - Error (500) al loguearse: ${err}`)
+			return res.status(500).send({ message: `Error al logearse: ${err}` })
+		}
+		if (!user) {
+			logger.error(`logIn - Error (404) al loguearse, email o psw invalidos: ${err}`)
+			return res.status(404).send({message: `El mail o la contraseña son invalidos`})
+		}
 		
 		let usuarioId = user._id
 		let newToken = service.createToken(user)
 		let update = {token: newToken}
 
 		User.findByIdAndUpdate(usuarioId, update, (err,usuarioUpdated)=>{
-			if(err) res.status(500).send({message:`Error al guardar el token del usuario: ${err}`})
+			if(err) {
+				logger.error(`logIn - Error (500) al guardar el token del usuario: ${err}`)
+				return res.status(500).send({message:`Error al guardar el token del usuario: ${err}`})
+			}
+			logger.info(`logIn - Se actualizo el token del usuario ${user.email}`)
 		})
+		logger.info(`logIn - Se logueo el usuario ${user.email}`)
 		return res.status(200).send({ message: 'Te has logueado correctamente', 
 			token: newToken,
 			name: user.name,
@@ -103,9 +117,14 @@ function logIn (req, res) {
 
 function getUserProfile(req, res) {
 	User.findOne({email: req.params.email}, (err, user) =>{
-		if(err) return res.status(500).send({message: `Error al buscar informacion del usuario: ${err}`})
-		if(!user) return res.status(400).send({message: 'El usuario solicitado no existe'})
+		if(err) {
+			logger.error(`getUserProfile - Error (500) al buscar informacion del usuario ${req.params.email}: ${err}`)
+			return res.status(500).send({message: `Error al buscar informacion del usuario: ${err}`})}
+		if(!user) {
+			logger.error(`getUserProfile - Error (400), el usuario ${req.params.email} no existe`)
+			return res.status(400).send({message: 'El usuario solicitado no existe'})}
 
+		logger.info(`getUserProfile - Se devolvió el perfil del usuario ${user.email}`)
 		return res.status(200).send({ name: user.name,
 			nickname: user.nickname,
 			email: user.email,
@@ -118,8 +137,11 @@ function updateUserProfile(req, res){
 	let userToken = req.body.token
 	let update = req.body
 	User.update({token: userToken}, update, (err,userUpdated)=>{
-		if(err) res.status(500).send({message:`Error al actualizar el perfil del usuario: ${err}`})
+		if(err) {
+			logger.error(`updateUserProfile - Error (500) al actualizar el perfil del usuario: ${err}`)
+			res.status(500).send({message:`updateUserProfile - Error al actualizar el perfil del usuario: ${err}`})}
 
+		logger.info(`updateUserProfile - El perfil del usuario ${userUpdated.email} se modificó correctamente`)
 		res.status(200).send({message: 'El perfil se modificó correctamente'})
 	})
 }
